988bd16231a32976112783526568a3aa06b4059b
Lucas Cardinot da Silva <116500373+UserCardinot@users.noreply.github.com>
2023-12-05 14:23:39 -0300

    Adição do crud de Cursos + trat erros

--- src/index.js
+++ src/index.js
@@ -14,10 +14,12 @@ const auth = require('./helpers/Auth.js')
 const sessionRouter = require('./control/SessionAPI')
 const adminRouter = require('./control/AdminAPI')
 const usersRouter = require('./control/UsersAPI')
+const cursosRouter = require('./control/CursosAPI')
 
 app.use("/session", sessionRouter)
 app.use("/user", auth.validarAcesso, usersRouter)
 app.use("/admin", auth.validarAcesso, auth.verificaAdmin, adminRouter)
+app.use("/cursos", auth.validarAcesso, cursosRouter)
 
 app.listen(process.env.PORT, () => {
     console.log("Listening on port " + process.env.PORT + "...")
---
+++ src/models/cursos.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose")
+
+const CursosSchema = new mongoose.Schema({
+    titulo: String,
+    descricao: String,
+    cargaHoraria: String,
+    categoria: String,
+    dataInicio: Date,
+    dataFim: Date,
+    url: String,
+})
+
+const CursosModel = mongoose.model("Curso", CursosSchema)
+
+module.exports = {
+    list: async function() {
+        const cursos = await CursosModel.find({}).lean()
+        return cursos
+    },
+
+    save: async function(titulo, descricao, cargaHoraria, categoria, dataInicio, dataFim, url) {
+        const cursos = new CursosModel({
+            titulo: titulo,
+            descricao: descricao,
+            cargaHoraria: cargaHoraria,
+            categoria: categoria,
+            dataInicio: dataInicio,
+            dataFim: dataFim,
+            url: url,
+        })
+        await cursos.save()
+        return cursos
+    },
+
+    update: async function(id, obj) {
+
+        let cursos = await CursosModel.findById(id)
+        if (!cursos) {
+            return false
+        }
+        
+        Object.keys(obj).forEach(key => cursos[key] = obj[key])
+        await cursos.save()
+        return cursos
+    },
+    
+    delete: async function(id) {
+        return await CursosModel.findByIdAndDelete(id)
+    },
+
+    getById: async function(id) {
+        return await CursosModel.findById(id).lean()
+    },
+
+    getByTitulo: async function(titulo){
+        return await CursosModel.findOne({titulo: titulo}).lean()
+    }
+}
