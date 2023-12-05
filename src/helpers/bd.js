dedf243f7fcbd2133030d418846d8040773d8aa2
Lucas Cardinot da Silva <116500373+UserCardinot@users.noreply.github.com>
2023-12-04 21:18:11 -0300

    hh

--- src/helpers/bd.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose")
-
-module.exports = (req, res, next) => {
-    mongoose.connect(process.env.MONGO_URI).catch((err) => {
-        console.log("Error ao conectar no banco...")
-    })
-    return next()    
-}
